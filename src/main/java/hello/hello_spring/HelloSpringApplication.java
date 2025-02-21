0e457d6c4c1794156eb7fb1546b70d2898b8f5ad
YoonHan0 <dbsgksdud12@naver.com>
2025-02-21 15:05:53 +0900

    md파일 정리

--- src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -3,13 +3,14 @@ package hello.hello_spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.*;
+
 @SpringBootApplication
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(HelloSpringApplication.class, args);
-		System.out.println("Hello World!");
 
+		System.out.println("Hello World!");
 	}
 }
