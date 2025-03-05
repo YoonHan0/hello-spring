211fec5f8597d6d67c49ca17449dafb9c6d1ff07
YoonHan0 <dbsgksdud12@naver.com>
2025-03-05 18:02:44 +0900

    인터페이스 완벽 이해하기

--- src/main/java/hello/hello_spring/HelloSpringApplication.java
+++ src/main/java/hello/hello_spring/HelloSpringApplication.java
@@ -15,16 +15,6 @@ public class HelloSpringApplication {
 
 		System.out.println("Hello World!");
 
-		Dog d = new Dog(28, "BaekSam");
-		d.eat();	//  부모(추상) 클래스로 부터 상속받은 추상 메소드를 직접 구현한 메소드를 실행
-		d.walk();
-		d.run();	// 부모(추상) 클래스의 인스턴스 메소드 실행
-		System.out.println("age: " + d.age + " name: " + d.name);
 
-		Cat c = new Cat(26, "Naong");
-		c.eat();
-		c.walk();
-		c.run();
-		System.out.println("age: " + c.age + " name: " + c.name);
 	}
 }
